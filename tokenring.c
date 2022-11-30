2f901dca9825503d9c983c4967e8a997a67760a5
sirkotal <93833262+sirkotal@users.noreply.github.com>
2022-11-30 12:26:52 +0000

    Merging beta branch into the main branch
    * Improved error messages
    
    * Added comments and working on srand()
    
    * Removed malloc redundancy - sizeof(char) is 1
    
    * srand() working reliably
    
    * Fixed bug on the first pipe communication

--- tokenring.c
+++ tokenring.c
@@ -1,100 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
 #include <stdbool.h>
 #include <time.h>
 
 int main (int argc, char *argv[]) {
-    /* check number of args */
+    // check number of args (must be 4)
     if(argc != 4) {
         printf("Couldn't execute the program\n");
         return EXIT_FAILURE;
     }
 
-    char* pipe_name = malloc(sizeof(char)*50);
-    int n_pipes = atoi(argv[1]);
-    double chance = atof(argv[2]);
-    int sleeper = atoi(argv[3]);
+    char* pipe_name = malloc(60);   // allocates memory for the pipe names
+    int n_pipes = atoi(argv[1]);                 // number of pipes
+    double chance = atof(argv[2]);               // chance of locking
+    int sleeper = atoi(argv[3]);                 // time it takes to unlock
 
-    int prob = chance * 100;
+    int prob = chance * 100;                     // 1 <= prob <= 100
 
-    int msg = 0;
+    int msg = 0;                                 // token value
 
 
-    for (int i = 1; i <= n_pipes; i++) {
+    for (int i = 1; i <= n_pipes; i++) {         // names every pipe
         if(i == n_pipes)
             sprintf(pipe_name, "pipe%dto1", i);
         else
             sprintf(pipe_name, "pipe%dto%d", i, i+1);
 
-        if ((mkfifo(pipe_name, 0666)) == -1) {
-            fprintf(stderr, "mkfifo() Error %s", strerror(errno));
+        if ((mkfifo(pipe_name, 0666)) == -1) {     // creates the FIFO special files (pipes)
+            perror("mkfifo");
             exit(0);
         }
     }
     
-    free(pipe_name);
+    free(pipe_name);                               // free pipe name memory (making sure we have enough memory for read_pipe/write_pipe allocations)
 
-    pid_t pids[n_pipes];
+    pid_t pids[n_pipes];                           // array of processes
 
-    char* write_pipe = malloc(sizeof(char)*50);
-    char* read_pipe = malloc(sizeof(char)*50);
+    char* read_pipe = malloc(60);
+    char* write_pipe = malloc(60);
 
     for (int i = 1; i <= n_pipes; i++) {                  // creates processes
         if ((pids[i-1] = fork()) < 0) {
             perror("fork");
             abort();
         } 
-        else if (pids[i-1] == 0) {
+        else if (pids[i-1] == 0) {                       // identifies the write and read pipes
             if (i == 1) {
                 sprintf(write_pipe, "pipe%dto%d", i, i+1);
                 sprintf(read_pipe, "pipe%dto1", n_pipes);
             } 
             else if (i == n_pipes) {
-                sprintf(write_pipe, "pipe%dto1", i);
                 sprintf(read_pipe, "pipe%dto%d", i-1, i);
+                sprintf(write_pipe, "pipe%dto1", i);
             } 
             else {
-                sprintf(write_pipe, "pipe%dto%d", i, i+1);
                 sprintf(read_pipe, "pipe%dto%d", i-1, i);
+                sprintf(write_pipe, "pipe%dto%d", i, i+1);
             }
 
-            srand(time(NULL) - (2*i));
 
             // array to store the pipes
             int pipeline[2];
 
             if (i == 1) {
                 if ((pipeline[i] = open(write_pipe, O_WRONLY)) < 0) {
-                    fprintf(stderr, "open() Error %s", strerror(errno));
+                    perror("open");
                     exit(0);
                 }
 
-                msg++;
-
                 if (write(pipeline[i], &msg, sizeof(int)) < 0) {
-                    fprintf(stderr, "write() Error %s", strerror(errno));
+                    perror("write");
                     exit(0);
                 }
 
                 close(pipeline[i]);
             }
 
+            srand(time(NULL) - i);      // seed for random
+
             while(true) {
 
                 // retrieves value from the previous process
                 if ((pipeline[0] = open(read_pipe, O_RDONLY)) < 0) {
-                    fprintf(stderr, "open() Error %s", strerror(errno));
+                    perror("open");
                     exit(0);
                 }
 
                 if (read(pipeline[0], &msg, sizeof(int)) < 0) {
-                    fprintf(stderr, "read() Error %s", strerror(errno));
+                    perror("read");
                     exit(0);
                 }
 
@@ -103,9 +102,9 @@ int main (int argc, char *argv[]) {
                 msg++; 
                 
                 // randomizes chances for lock/unlock process
-                int rand= random() % 100 + 1;
+                int num = rand() % 100 + 1;
 
-                if (rand <= prob) {
+                if (num <= prob) {
                     printf("[p%d] lock on token (val = %d)\n", i, msg);
                     sleep(sleeper);
                     printf("[p%d] unlock token\n", i);
@@ -113,12 +112,12 @@ int main (int argc, char *argv[]) {
 
                 // writing the value of the message into the next process
                 if((pipeline[1] = open(write_pipe, O_WRONLY)) < 0) {
-                    fprintf(stderr, "open() Error %s", strerror(errno));
+                    perror("open");
                     exit(0);
                 }
                 
                 if(write(pipeline[1], &msg, sizeof(int)) < 0) {
-                    fprintf(stderr, "write() Error %s", strerror(errno));
+                    perror("write");
                     exit(0);
                 }
 
@@ -131,7 +130,7 @@ int main (int argc, char *argv[]) {
 
     for (int i = 0; i < n_pipes; i++) {
         if(waitpid(pids[i], NULL, 0) < 0) {
-            fprintf(stderr, "waitpid() Error %s", strerror(errno));
+            perror("waitpid");
             return EXIT_FAILURE;
         }
     }
